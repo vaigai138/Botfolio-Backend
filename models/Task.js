ee4738517c3185cada51238aa8ae6e2e5a18164a
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2025-08-12 11:36:33 +0530

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -29,6 +29,14 @@ const taskSchema = new mongoose.Schema({
     required: true,
     default: 0,
   },
+  paymentMethod: { // New payment method field
+    type: String,
+    enum: ['Bank Transfer', 'Cash', 'UPI', 'PayPal', 'Other'], // Enforce a predefined list of values
+    default: 'Bank Transfer',
+  },
+  paymentReceivedDate: { // New payment received date field
+    type: Date,
+  },
   received: { // New field
     type: Boolean,
     default: false,
@@ -37,3 +45,4 @@ const taskSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 export default mongoose.model('Task', taskSchema);
+
